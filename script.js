92843deaf8988fc368765a1cfa22d9d41f751558
danilagamba <mds@mds.com>
2024-07-03 12:37:50 -0500

    subir codigo

---
+++ script.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+document.getElementById('loginButton').addEventListener('click', function() {
+    // Redirigir a la página de inicio de sesión
+    window.location.href = 'login.html';
+});
+
+document.getElementById('registerButton').addEventListener('click', function() {
+    // Redirigir a la página de registro
+    window.location.href = 'register.html';
+});
